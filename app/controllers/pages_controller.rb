8aa1486dac3ed3ca624cf7c69f97f42808237d3f
Mike Spizzirri <ragnarisxx@gmail.com>
2015-06-18 04:11:33 +0000

    Linked navbar to pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def home
   end
+  def about
+  end
+  def academy
+  end
+  def allthatsleft
+  end
+  def beacon
+  end
+  def befuddling
+  end
+  def donate
+  end
+  def limitbreaking
+  end
+  def news
+  end
+  def popatropalis
+  end
 end
